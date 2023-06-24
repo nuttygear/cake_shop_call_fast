99c7a24ee262b5166af12b61561ec112c04d04ed
nuttygear <chanintorn.comsci@gmail.com>
2023-06-24 20:01:52 +0700

    1st commit...

---
+++ lib/views/show_shop_ui.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ShowShopUI extends StatefulWidget {
+  const ShowShopUI({super.key});
+
+  @override
+  State<ShowShopUI> createState() => _ShowShopUIState();
+}
+
+class _ShowShopUIState extends State<ShowShopUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
